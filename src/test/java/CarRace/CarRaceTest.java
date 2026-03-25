dac3051e8b4ed7421c758b35cafb60276b442109
mikeylili <a01073111145@gmail.com>
2026-03-25 14:03:36 +0900

    test: n대의 자동차에 이름 부여하면서 우승자 구하기 테스트, 우승자가 여러명일때 테스트

--- src/test/java/CarRace/CarRaceTest.java
+++ src/test/java/CarRace/CarRaceTest.java
@@ -3,6 +3,7 @@ package CarRace;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -16,9 +17,9 @@ public class CarRaceTest {
         //given
         CarRace carrace = new CarRace();
         //when
-        String result = carrace.GoOrStop(value);
+        int result = carrace.GoOrStop(value);
         //then
-        assertThat(result).isEqualTo("CCar Stop");
+        assertThat(result).isEqualTo(0);
     }
 
     @DisplayName("자동차가 랜덤값이 4이상이면 움직인다.")
@@ -28,9 +29,9 @@ public class CarRaceTest {
         //given
         CarRace carrace = new CarRace();
         //when
-        String result = carrace.GoOrStop(value);
+        int result = carrace.GoOrStop(value);
         //then
-        assertThat(result).isEqualTo("CCar Go");
+        assertThat(result).isEqualTo(1);
     }
 
     @DisplayName("적절한 범위의 랜덤값을 제공한다.")
@@ -43,5 +44,29 @@ public class CarRaceTest {
         boolean result = (n >= 0) && (n <= 9);
         //then
         assertThat(result).isTrue();
+    }
+    @DisplayName("자동차가 여러대일때 우승자를 구한다.")
+    @Test
+    void testGetWinner() {
+        //given
+        CarRace carrace = new CarRace();
+        //when
+        int[] carsscores = {4,2,6,8,5};
+        String maxscorecar = carrace.GetWinner(carsscores);
+        //then
+        assertThat(maxscorecar).isEqualTo("CCar3");
+
+    }
+    @DisplayName("자동차가 여러대일때 우승자가 여러명일때 우승자를 구한다.")
+    @Test
+    void testGetWinners() {
+        //given
+        CarRace carrace = new CarRace();
+        //when
+        int[] carsscores = {4,2,8,8,5};
+        String maxscorecar = carrace.GetWinner(carsscores);
+        //then
+        assertThat(maxscorecar).isEqualTo("CCar2, CCar3");
+
     }
 }
