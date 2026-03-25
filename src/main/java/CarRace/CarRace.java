5dc1a105a26293456c738bffbe9e0d2589f62869
mikeylili <a01073111145@gmail.com>
2026-03-25 14:01:51 +0900

    feat: n대의 자동차, 자동차 우승자 구하기 구현

--- src/main/java/CarRace/CarRace.java
+++ src/main/java/CarRace/CarRace.java
@@ -1,27 +1,46 @@
 package CarRace;
 
+import java.util.Comparator;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class CarRace {
-    public String name = "CCar";
-    public int GenerateRandom0to9(){
+
+    public int GenerateRandom0to9() {
         int move = (int) (Math.random() * 10);
         return move;
     }
 
-    public String GoOrStop(int move) {
+    public int GoOrStop(int move) {
 
         if (move >= 4) {
-            System.out.println(name + " Go");
-            return name + " Go";
+            //System.out.println(" Go");
+            return 1;
         }
         if (move <= 3) {
-            System.out.println(name + " Stop");
-            return name + " Stop";
+            //System.out.println(" Stop");
+            return 0;
         }
-        throw new RuntimeException();
+        throw new RuntimeException("배열에 3초과 4미만인 수가 들어있습니다.");
     }
 
-    public String Racing() {
-        int move = GenerateRandom0to9();
-        return GoOrStop(move);
+    public int[] Racing(int n) {
+        int[] carscores = new int[n];
+        int maxScore = 0;
+        for (int i = 0; i < n; i++) {
+            String name = "CCar" + i;
+            int move = GenerateRandom0to9();
+            carscores[i] += GoOrStop(move);
+        }
+        return carscores;
+    }
+
+    public String GetWinner(int[] carscores) {
+        int maxScore = IntStream.of(carscores).max().orElse(0);
+        String maxIndexes = IntStream.range(0, carscores.length)
+                .filter(i -> carscores[i] == maxScore)
+                .mapToObj(i -> "CCar" + i)
+                .collect(Collectors.joining(", "));
+        return maxIndexes;
     }
 }
